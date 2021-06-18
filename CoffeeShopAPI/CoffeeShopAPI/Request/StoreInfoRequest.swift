d12f323c0b4f2af4bef01980debfce9e3c3627a3
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-18 12:05:08 +0700

    refactor: use baseURL in StoreInfoRequest

--- CoffeeShopAPI/CoffeeShopAPI/Request/StoreInfoRequest.swift
+++ CoffeeShopAPI/CoffeeShopAPI/Request/StoreInfoRequest.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public enum StoreInfoRequest {
   public static func request(completion: @escaping (Result<StoreInfo, RequestError>) -> Void) {
-    let url = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/storeInfo")!
+    let url = Configs.baseURL.appendingPathComponent("/storeInfo")
 
     let task = URLSession.shared.dataTask(with: url) { data, _, error in
       if let error = error {
