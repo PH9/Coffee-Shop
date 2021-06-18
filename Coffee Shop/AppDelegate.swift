3b6aba6f6fd70f585fd5e277fd407be1ed69aa18
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-18 10:35:31 +0700

    refactor: rerun swiftformat

--- Coffee Shop/AppDelegate.swift
+++ Coffee Shop/AppDelegate.swift
@@ -3,7 +3,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_: UIApplication,
-                   didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+                   didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+  {
     // Override point for customization after application launch.
     return true
   }
