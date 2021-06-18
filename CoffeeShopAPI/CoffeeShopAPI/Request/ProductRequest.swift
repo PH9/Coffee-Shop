fb37c111017a5b5d631691a3b9c8b7dc497cf00a
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-18 11:44:58 +0700

    feat: intial ProductRequest

---
+++ CoffeeShopAPI/CoffeeShopAPI/Request/ProductRequest.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public enum ProductRequest {
+  public static func request(completion: @escaping (Result<[Product], RequestError>) -> Void) {
+    let url = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/products")!
+
+    let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+      guard let data = data else {
+        completion(.failure(RequestError.onData))
+        return
+      }
+
+      do {
+        let value = try JSONDecoder().decode([Product].self, from: data)
+        completion(.success(value))
+      } catch {
+        completion(.failure(.parsing(error)))
+      }
+    }
+
+    task.resume()
+  }
+}
