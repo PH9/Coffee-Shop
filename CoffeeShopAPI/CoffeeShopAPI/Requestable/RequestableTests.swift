67c506f2e092343f0176ec542d895f7b78307289
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-18 14:21:11 +0700

    test: add test for got error response should return error

--- CoffeeShopAPI/CoffeeShopAPI/Requestable/RequestableTests.swift
+++ CoffeeShopAPI/CoffeeShopAPI/Requestable/RequestableTests.swift
@@ -27,7 +27,35 @@ class RequestableTests: XCTestCase {
       case .responseIsNotHTTPURLResponse:
         break
       default:
-        XCTFail("Should be responseIsNotHTTPURLResponse error")
+        XCTFail("Should be responseIsNotHTTPURLResponse, but got \(error)")
+      }
+    }
+
+    wait(for: [expectation], timeout: 1)
+  }
+
+  func test_request_withErrorResponse_shouldGetErrorResult() {
+    let error = NSError(domain: "com.ph9", code: -99, userInfo: nil)
+    URLProtocolMock.stub = [sut.url: (nil, nil, error)]
+
+    let expectation = XCTestExpectation()
+    _ = sut.request(urlSession: makeMockSession()) { result in
+      defer {
+        expectation.fulfill()
+      }
+
+      guard case let .failure(responseError) = result else {
+        XCTFail("There is no nothing, should get failure result")
+        return
+      }
+
+      switch responseError {
+      case let .dataTask(responseError):
+        let responseError = responseError as NSError
+        XCTAssertEqual(responseError.code, error.code)
+        XCTAssertEqual(responseError.domain, error.domain)
+      default:
+        XCTFail("Should be responseIsNotHTTPURLResponse, but got \(responseError)")
       }
     }
 
