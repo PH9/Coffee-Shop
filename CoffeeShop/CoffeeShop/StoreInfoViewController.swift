194240202412ad50f551a5581f916219394a15e3
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-19 13:09:26 +0700

    feat: draft shop's profile

--- CoffeeShop/CoffeeShop/StoreInfoViewController.swift
+++ CoffeeShop/CoffeeShop/StoreInfoViewController.swift
@@ -1,3 +1,7 @@
 import UIKit
 
-class StoreInfoViewController: UIViewController {}
+class StoreInfoViewController: UIViewController {
+  @IBOutlet var storeNameLabel: UILabel!
+  @IBOutlet var ratingLabel: UILabel!
+  @IBOutlet var workHourLabel: UILabel!
+}
