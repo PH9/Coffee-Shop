e25c1a6f63290ca8777be0185a370d504736e16b
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-19 16:52:42 +0700

    feat: connect store info to API and view

--- CoffeeShop/CoffeeShop/Order/OrderViewController.swift
+++ CoffeeShop/CoffeeShop/Order/OrderViewController.swift
@@ -1,3 +1,8 @@
 import UIKit
 
-public final class OrderViewController: UIViewController {}
+public final class OrderViewController: UIViewController {
+  override public func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(true, animated: false)
+  }
+}
--- CoffeeShop/CoffeeShop/StoreInfo/StoreInfoViewController.swift
+++ CoffeeShop/CoffeeShop/StoreInfo/StoreInfoViewController.swift
@@ -1,7 +1,39 @@
+import CoffeeShopAPI
 import UIKit
 
 final class StoreInfoViewController: UIViewController {
   @IBOutlet var storeNameLabel: UILabel!
   @IBOutlet var ratingLabel: UILabel!
   @IBOutlet var workHourLabel: UILabel!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    renderLoading()
+    getStoreInfo()
+  }
+
+  private func renderLoading() {
+    storeNameLabel.text = "Loading..."
+    ratingLabel.text = ""
+    workHourLabel.text = ""
+  }
+
+  private func getStoreInfo() {
+    StoreInfoRequest().request { [weak self] result in
+      switch result {
+      case let .success(info):
+        DispatchQueue.main.async {
+          self?.render(info)
+        }
+      default:
+        break
+      }
+    }
+  }
+
+  private func render(_ info: StoreInfo) {
+    storeNameLabel.text = info.name
+    ratingLabel.text = "\(info.rating)/5"
+    workHourLabel.text = "Opening hour: \(info.openingTime) - \(info.closingTime)"
+  }
 }
