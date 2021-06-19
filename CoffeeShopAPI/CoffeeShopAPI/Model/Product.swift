c6d0bdd6aae44f59621a2b3673770454862309d6
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-19 14:30:42 +0700

    fix: key is wrong

--- CoffeeShopAPI/CoffeeShopAPI/Model/Product.swift
+++ CoffeeShopAPI/CoffeeShopAPI/Model/Product.swift
@@ -4,4 +4,10 @@ public struct Product: Codable {
   let name: String
   let price: Int
   let imageURL: URL
+
+  private enum CodingKeys: String, CodingKey {
+    case name
+    case price
+    case imageURL = "imageUrl"
+  }
 }
