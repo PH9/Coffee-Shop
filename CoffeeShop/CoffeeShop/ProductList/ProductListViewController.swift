69497c3f62bd51cb180c0196e84de13be52f9eb1
Wasith 'Pi' Theerapattrathamrong <wasith.t@gmail.com>
2021-06-19 14:10:30 +0700

    fix: table can return cell now

--- CoffeeShop/CoffeeShop/ProductList/ProductListViewController.swift
+++ CoffeeShop/CoffeeShop/ProductList/ProductListViewController.swift
@@ -1,3 +1,11 @@
 import UIKit
 
-final class ProductListViewController: UITableViewController {}
+final class ProductListViewController: UITableViewController {
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath) as? ProductCell else {
+      fatalError("Could not dequeue cell")
+    }
+
+    return cell
+  }
+}
